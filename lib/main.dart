754a03cb8605579c365bcb8632770e7a14313e4e
Bibek bhattarai <bhattaraibvk@gmail.com>
2020-09-27 21:33:27 +0545

    work

--- lib/main.dart
+++ lib/main.dart
@@ -3,13 +3,11 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-     
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
@@ -17,6 +15,7 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// work done
 class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -27,115 +26,71 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xFF21BFBD),
-      body: ListView(
-        children: <Widget>[
-         Padding(padding: EdgeInsets.all( 15.0),
-         child:Row(
-           mainAxisAlignment: MainAxisAlignment.spaceAround,
-             children: [
-               IconButton(
-                 icon: Icon(Icons.arrow_back_ios),
-                 color: Colors.white,
-                  onPressed: (){},
-               ),
-               
-               Container(
-                  width:125.10,
+      body: ListView(children: <Widget>[
+        Padding(
+          padding: EdgeInsets.all(15.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                icon: Icon(Icons.arrow_back_ios),
+                color: Colors.white,
+                onPressed: () {},
+              ),
+              Container(
+                  width: 125.10,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: <Widget>[
-                  IconButton(
-                    icon: Icon(Icons.filter_list),
-                    color: Colors.white,
-                    onPressed: (){},
-                  ),
-
-                  IconButton(
-                    icon: Icon(Icons.menu),
-                    color: Colors.white,
-                    onPressed: (){},
-                  ),
-                  ],)
-
-
-               )
-               
-
-
-
-
-             ],
-
-
-         ) ,
-         
-         
-         ),
-
-         Padding(
-           padding: EdgeInsets.only(left: 40.0),
-           child: Row(
-             children: <Widget>[
-              Text("Healthy",
-              style:TextStyle(
-                color: Colors.red,
-                fontSize:20,
-              )),
-             SizedBox(width:10),
-              Text("Food",
-              style:TextStyle(
-                color:Colors.white,
-                fontSize:20
-              ))
-
-
-             ]
-
-
-           ),
-           
-            ),
-          
-           SizedBox(height:20),
-    Container(
-   height:MediaQuery.of(context).size.height - 185,
-   decoration: BoxDecoration(
-     color: Colors.white,
-     borderRadius: BorderRadius.only(topLeft: Radius.circular(75.0)),
-   ),
-   child: ListView(
-     primary:false,
-     padding: EdgeInsets.only(left: 25.0, right: 20.0),
-
-     children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.filter_list),
+                        color: Colors.white,
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.menu),
+                        color: Colors.white,
+                        onPressed: () {},
+                      ),
+                    ],
+                  ))
+            ],
+          ),
+        ),
         Padding(
-          child:Container(
-           height: MediaQuery.of(context).size.height - 300.0,
-           child: ListView(
-                 children: [
-
-                 ],
-           ),
-
-
-          ) ,)
-
-     ]
-
-   ),
-
-    
-     
- 
-
-    )
-          
-        ]
-      ),
-      
+          padding: EdgeInsets.only(left: 40.0),
+          child: Row(children: <Widget>[
+            Text("Healthy",
+                style: TextStyle(
+                  color: Colors.red,
+                  fontSize: 20,
+                )),
+            SizedBox(width: 10),
+            Text("Food", style: TextStyle(color: Colors.white, fontSize: 20))
+          ]),
+        ),
+        SizedBox(height: 20),
+        Container(
+          height: MediaQuery.of(context).size.height - 185,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(75.0)),
+          ),
+          child: ListView(
+              primary: false,
+              padding: EdgeInsets.only(left: 25.0, right: 20.0),
+              children: <Widget>[
+                Padding(
+                  child: Container(
+                    height: MediaQuery.of(context).size.height - 300.0,
+                    child: ListView(
+                      children: [],
+                    ),
+                  ),
+                )
+              ]),
+        )
+      ]),
     );
   }
 }
-
-
-
